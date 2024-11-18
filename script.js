97080f84d33f32e1d4650309a00765c0c0869b31
InSungChang <changtecnologia@gmail.com>
2024-11-18 11:19:04 -0300

    Adicionado mais itens para Tablet e PC

--- script.js
+++ script.js
@@ -47,16 +47,20 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     function updateText(clickedIndex) {
-        const titles = ["Identidade Visual", "Social Media Strategy", "Social Media", "Audio Visual", "Google ADS", "Meta ADS", "Aguarde, vem aí mais novidades!", "Aguarde, vem aí mais novidades!"];
-        const subtitles = ["saiba mais.", "saiba mais.", "saiba mais.", "saiba mais.", "saiba mais.", "saiba mais.", "saiba mais.", "saiba mais."];
+        const titles = ["Identidade Visual", "Social Media Strategy", "Social Media", "Audio Visual", "Google ADS", "Meta ADS", "UX design", "Desenvolvimento de Aplicação Web", "A análise de dados", "Suporte técnico", "Infraestrutura", "Marcas e Patentes"];
+        const subtitles = ["saiba mais.", "saiba mais.", "saiba mais.", "saiba mais.", "saiba mais.", "saiba mais.", "saiba mais.", "saiba mais.", "saiba mais.", "saiba mais.", "saiba mais.", "saiba mais."];
         const descricoes = ["Através de um design criativo e estratégico, transformamos sua marca em uma experiência que conecta com seu público, de forma única e personalizada.",
             "Desenhamos um plano de ação nas redes sociais que vai além da simples publicação de posts. Combinamos estratégia, criatividade e dados para construir uma comunidade engajada e impulsionar seus resultados de negócio.",
             "Nosso processo transforma sua marca em uma história nas redes sociais. Com estratégia, conteúdo criativo e design único.",
             "Sua marca ganha vida através de vídeos criativos e impactantes. Produzimos conteúdo audiovisual que reflete a identidade da sua marca.",
             "Com uma análise completa do mercado, criamos campanhas personalizadas no Google, monitoradas de perto para que seu produto seja visto por mais pessoas.",
-            "Meta ADS",
-            "Aguarde, vem aí mais novidades para você!!!",
-            "Aguarde, vem aí mais novidades para você!!!"]
+            "Através de uma análise estratégica, criaremos campanhas personalizadas nas redes sociais Facebook e Instagram, visando aumentar a visibilidade do seu perfil e do seu produto.",
+            "Desenhamos e programamos experiências que encantam e facilitam a jornada do usuário, alinhando estética e funcionalidade. Focamos em entender o fundo das necessidades e desejos do seu público, para que cada interação seja intuitiva, prática, agradável e criativa. Com uma abordagem focada no usuário, buscamos criar conexões reais entre marcas e pessoas, tornando cada contato único e simultaneamente. E esse site é o maior exemplo de tudo isso ;)",
+            "Criação, manutenção e atualização de sistema baseado na web. São programas que é executado na internet, ou seja, os arquivos e dados trabalhados são processados e armazenados na internet. Não é necessário instalar o aplicativo no computador.",
+            "Resolução de problemas técnicos e um atendimento dedicado aos usuários. Com isso, garantimos que dúvidas e dificuldades sejam solucionadas de forma rápida e eficiente, proporcionando uma experiência mais tranquila e produtiva.",
+            "O suporte técnico oferece a resolução de problemas técnicos e um atendimento dedicado aos usuários. Com isso, garantimos que dúvidas e dificuldades sejam solucionadas de forma rápida e eficiente, proporcionando uma experiência mais tranquila e produtiva.",
+            "Com uma infraestrutura sólida e atualizada, a empresa ganha em eficiência operacional e consegue prevenir problemas técnicos, além de oferecer uma base confiável para o crescimento e inovação tecnológica.",
+            "Marcas e Patentes"];  
 
         title.textContent = titles[clickedIndex];
         subtitle.textContent = subtitles[clickedIndex];
@@ -68,11 +72,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Animação inicial
     const initialRotations = 1; // Número de rotações completas
-    const initialDuration = 2000; // Duração total da animação em milissegundos
+    const initialDuration = 4000; // Duração total da animação em milissegundos
     const finalIndex = 0; // Índice do número 00
 
     circle.style.transition = `transform ${initialDuration}ms ease-in-out`;
-    circle.style.transform = `rotate(${-360 * initialRotations}deg)`;
+    circle.style.transform = `rotate(${-180 * initialRotations}deg)`;
 
     numbers.forEach((number) => {
         number.style.transition = `transform ${initialDuration}ms ease-in-out`;
